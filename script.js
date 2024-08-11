174cffbf5b7bbb485b058d7f73574557cb18ca7a
Harshil Chavda <151707147+Harshil-Chavda@users.noreply.github.com>
2024-08-11 20:27:59 +0530

    Commited

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
-consoleText(['Created By', 'HK Chavda.'], 'text',['tomato','rebeccapurple','lightblue']);
+consoleText(['Created By', 'Harshil Chavda.'], 'text',['tomato','rebeccapurple','lightblue']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
